d9636dff03e0576489bdb752529ff9a1d3efeaf8
diseño y desarrollo <disenoydesarrollo@academicos.cesde.edu.co>
2024-09-10 20:33:36 -0500

    NUEVA UI / FALTA HOME Y ACTIVTY MAIN

--- app/src/main/java/com/david/ecommerce_mobile/registro.java
+++ app/src/main/java/com/david/ecommerce_mobile/registro.java
@@ -8,23 +8,10 @@ import android.view.View;
 import android.widget.Button;
 
 public class registro extends AppCompatActivity {
-    Button btn_home;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registro);
 
-        btn_home = findViewById(R.id.btn_home);
-        btn_home.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                irHome();
-            }
-        });
-    }
-
-    private void irHome(){
-        Intent intent = new Intent(registro.this, home.class);
-        startActivity(intent);
     }
 }
